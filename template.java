f137b123309fda6b14a7968d84974ecb4e30f6b8
KyleReto <40499747+KyleReto@users.noreply.github.com>
2021-12-03 18:21:16 -0500

    Updated template

--- template.java
+++ template.java
@@ -5,11 +5,12 @@ import java.nio.file.Paths;
 class Template {
     
     public static void main(String[] args) throws IOException{
+        String[] input = getInput(4);
     }
 
     // Given an input file, outputs an array of trimmed strings.
-    public static String[] getInput() throws IOException{
-        String textInput = Files.readString(Paths.get("./inputs/2.txt"));
+    public static String[] getInput(int day) throws IOException{
+        String textInput = Files.readString(Paths.get("./inputs/" + day + ".txt"));
         String[] inputs = textInput.split("\n");
         for (int i = 0; i < inputs.length; i++){
             inputs[i] = inputs[i].trim();
